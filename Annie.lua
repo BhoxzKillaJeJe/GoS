a6e8d11035b279910316fd545a46ee6fa8e87e60
GoodGame <roiz_limbo@yahoo.com>
2016-01-17 02:45:54 +0800

    Update Annie.lua

--- Annie.lua
+++ Annie.lua
@@ -36,11 +36,11 @@ menu.d:Boolean("dr", "Draw R", false)
 menu.d:Boolean("dmg", "Draw DMG", true)
 
 ---SpellData---------------
-Annie.SpellData = {
-	[_Q] = {dmg = function () return 45 + 35*GetCastLevel(myHero,_Q) + 0.8*GetBonusAP(myHero) end, range = 625, mana = function () return 55 + 5*GetCastLevel(myHero,_Q) end},
-	[_W] = {dmg = function () return 25 + 45*GetCastLevel(myHero,_W) + 0.85*GetBonusAP(myHero) end , range = 625, mana = function () return 60 + 10*GetCastLevel(myHero,_W) end},
-	[_E] = {dmg = function () return 10 + 10*GetCastLevel(myHero,_E) + 0.2*GetBonusAP(myHero) end , mana = 20 },
-	[_R] = {dmg = function () return 50 + 125*GetCastLevel(myHero,_R) + 0.8*GetBonusAP(myHero) end, range = 600, radius = 250, mana = 100 },
+SpellData = {
+	[_Q] = {dmg = function () return 45 + 35*GetCastLevel(myHero,_Q) + 0.8*GetBonusAP(myHero) end, range = 625},
+	[_W] = {dmg = function () return 25 + 45*GetCastLevel(myHero,_W) + 0.85*GetBonusAP(myHero) end , range = 625},
+	[_E] = {dmg = function () return 10 + 10*GetCastLevel(myHero,_E) + 0.2*GetBonusAP(myHero) end},
+	[_R] = {dmg = function () return 50 + 125*GetCastLevel(myHero,_R) + 0.8*GetBonusAP(myHero) end, range = 600, radius = 250,},
 	}
 ---------------------------
 
@@ -56,7 +56,7 @@ function Combo()
 	local Qtarget = ts:GetTarget()
 	if IOW:Mode() == "Combo" then
 
-		if IsReady(_R) and ValidTarget(target,Annie.SpellData[_R].range) and menu.c.cr:Value() and (GetCurrentHP(target)/GetMaxHP(target)) <= menu.c.hcr:Value() then
+		if IsReady(_R) and ValidTarget(target,SpellData[_R].range) and menu.c.cr:Value() and (GetCurrentHP(target)/GetMaxHP(target)) <= menu.c.hcr:Value() then
 			CastSkillShot(_R,GetOrigin(target))
 		end
 
@@ -64,11 +64,11 @@ function Combo()
 			CastSpell(_E)
 		end
 
-		if IsReady(_Q) and ValidTarget(Qtarget,Annie.SpellData[_Q].range) and menu.c.cq:Value() then
+		if IsReady(_Q) and ValidTarget(Qtarget,SpellData[_Q].range) and menu.c.cq:Value() then
 			CastTargetSpell(Qtarget,_Q)
 		end
 
-		if IsReady(_W) and ValidTarget(target,Annie.SpellData[_W].range) and menu.c.cw:Value()	then
+		if IsReady(_W) and ValidTarget(target,SpellData[_W].range) and menu.c.cw:Value()	then
 			CastSkillShot(_W,GetOrigin(target))
 		end
 	end 
@@ -79,11 +79,11 @@ function Harass()
 	local Qtarget = ts:GetTarget()
 	if IOW:Mode() == "Harass" then
 
-		if IsReady(_W) and ValidTarget(target,Annie.SpellData[_W].range) and menu.h.hw:Value() then
+		if IsReady(_W) and ValidTarget(target,SpellData[_W].range) and menu.h.hw:Value() then
 			CastSkillShot(_W,GetOrigin(target))
 		end
 
-		if IsReady(_Q) and ValidTarget(Qtarget,Annie.SpellData[_Q].range) and menu.h.hq:Value() then
+		if IsReady(_Q) and ValidTarget(Qtarget,SpellData[_Q].range) and menu.h.hq:Value() then
 			CastTargetSpell(Qtarget,_Q)
 		end
 	end
@@ -91,9 +91,8 @@ end
 
 function AutoR()
 	for i,enemy in pairs(GetEnemyHeroes()) do
-		local enemycount = CountObjectsNearPos(GetOrigin(enemy),Annie.SpellData[_R].range,Annie.SpellData[_R].radius - GetHitBox(enemy),GetEnemyHeroes())
 		
-		if IsReady(_R) and ValidTarget(enemy,Annie.SpellData[_R].range) and enemycount >= menu.c.acr:Value() and menu.c.cr:Value() and pstun == true then
+		if IsReady(_R) and ValidTarget(enemy,SpellData[_R].range) and EnemiesAround(GetOrigin(myHero),enemy) >= menu.c.acr:Value() and menu.c.cr:Value() and pstun == true then
 			CastSkillShot(_R,GetOrigin(enemy))
 		end
 	end
@@ -102,10 +101,10 @@ end
 function AutoQ()
 	if not IOW.isWindingUp then
 		for _,minions in pairs(minionManager.objects) do
-			if GetTeam(minions) == MINION_ENEMY and ValidTarget(minions,Annie.SpellData[_Q].range) and menu.m.aq:Value() then
+			if GetTeam(minions) == MINION_ENEMY and ValidTarget(minions,SpellData[_Q].range) and menu.m.aq:Value() then
 				local predhp = IOW:PredictHealth(minions, GetDistance(minions)*0.5+250)
 				if predhp > 0 then
-					if predhp+5 < CalcDamage(myHero,minions,0,Annie.SpellData[_Q].dmg) then
+					if predhp < CalcDamage(myHero,minions,0,SpellData[_Q].dmg()) then
 						CastTargetSpell(minions,_Q)
 					end
 				end
@@ -116,16 +115,16 @@ end
 
 function KillSteal()
 	for i,enemy in pairs(GetEnemyHeroes()) do
-		local Qdmg = CalcDamage(myHero,enemy,0,Annie.SpellData[_Q].dmg + Ludens())
-		local Wdmg = CalcDamage(myHero,enemy,0,Annie.SpellData[_W].dmg + Ludens())
-		local Rdmg = CalcDamage(myHero,enemy,0,Annie.SpellData[_R].dmg + Ludens())
+		local Qdmg = CalcDamage(myHero,enemy,0,SpellData[_Q].dmg() + Ludens())
+		local Wdmg = CalcDamage(myHero,enemy,0,SpellData[_W].dmg() + Ludens())
+		local Rdmg = CalcDamage(myHero,enemy,0,SpellData[_R].dmg() + Ludens())
 		local enemyhp = GetCurrentHP(enemy) + GetDmgShield(enemy) + GetMagicShield(enemy)
 
-		if ValidTarget(enemy,Annie.SpellData[_W].range) and Wdmg > enemyhp and menu.ks.ksw:Value() and IsReady(_W) then
+		if ValidTarget(enemy,SpellData[_W].range) and Wdmg > enemyhp and menu.ks.ksw:Value() and IsReady(_W) then
 			CastSkillShot(_W,GetOrigin(enemy))
-		elseif ValidTarget(enemy,Annie.SpellData[_Q].range) and Qdmg > enemyhp and menu.ks.ksq:Value() and IsReady(_Q) then
+		elseif ValidTarget(enemy,SpellData[_Q].range) and Qdmg > enemyhp and menu.ks.ksq:Value() and IsReady(_Q) then
 			CastTargetSpell(enemy,_Q)
-		elseif ValidTarget(enemy,Annie.SpellData[_R].range + 100) and Rdmg > enemyhp and menu.ks.ksr:Value() and IsReady(_R) then
+		elseif ValidTarget(enemy,SpellData[_R].range + 100) and Rdmg > enemyhp and menu.ks.ksr:Value() and IsReady(_R) then
 			CastSkillShot(_R,GetOrigin(enemy))
 		elseif ValidTarget(enemy,625) and Qdmg + Wdmg > enemyhp and menu.ks.ksq:Value() and menu.ks.ksw:Value() and IsReady(_Q) and IsReady(_W) then
 			CastTargetSpell(enemy,_Q) DelayAction(function() CastSkillShot(_W,GetOrigin(enemy)) end, 250)
@@ -134,7 +133,7 @@ function KillSteal()
 		elseif ValidTarget(enemy,625) and Rdmg + Wdmg > enemyhp and menu.ks.ksr:Value() and menu.ks.ksw:Value() and IsReady(_R) and IsReady(_W) then
 			CastSkillShot(_R,GetOrigin(enemy)) DelayAction(function() CastSkillShot(_W,GetOrigin(enemy)) end, 250)
 		elseif ValidTarget(enemy,625) and Qdmg + Wdmg + Rdmg > enemyhp and menu.ks.ksq:Value() and menu.ks.ksw:Value() and menu.ks.ksr:Value() and IsReady(_Q) and IsReady(_W) and IsReady(_R) then
-			CastTagetSpell(enemy,_Q) DelayAction(function() CastSkillShot(_W,GetOrigin(enemy)) DelayAction(function() CastSkillShot(_R,GetOrigin(enemy))end, 250) end, 250))
+			CastTargetSpell(enemy,_Q) DelayAction(function() CastSkillShot(_W,GetOrigin(enemy)) DelayAction(function() CastSkillShot(_R,GetOrigin(enemy))end, 250) end, 250)
 		end
 	end
 end
@@ -164,7 +163,7 @@ OnUpdateBuff(function(unit,buff)
 			LudensStacks = buff.Count
 		end
 	end
-end
+end)
 
 OnRemoveBuff(function(unit,buff)
 	if unit == myHero then
@@ -174,7 +173,7 @@ OnRemoveBuff(function(unit,buff)
 			pstun = false
 		end
 	end
-end
+end)
 
 OnTick(function(myHero)
  	if not IsDead(myHero) then
@@ -185,26 +184,29 @@ OnTick(function(myHero)
  		KillSteal()
  		AutoIgnite()
  	end
-end
+end)
 
 OnDraw(function(myHero)
 	local target = GetCurrentTarget()
-	local Qdmg = CalcDamage(myHero,target,0,Annie.SpellData[_Q].dmg + Ludens())
-	local Wdmg = CalcDamage(myHero,target,0,Annie.SpellData[_W].dmg + Ludens())
-	local Rdmg = CalcDamage(myHero,target,0,Annie.SpellData[_R].dmg + Ludens())
+	local Qdmg = CalcDamage(myHero,target,0,SpellData[_Q].dmg() + Ludens())
+	local Wdmg = CalcDamage(myHero,target,0,SpellData[_W].dmg() + Ludens())
+	local Rdmg = CalcDamage(myHero,target,0,SpellData[_R].dmg() + Ludens())
 	local enemyhp = GetCurrentHP(target) + GetDmgShield(target) + GetMagicShield(target)
-	local apdmg = Qdmg + Wdmg + Rdmg + Ludens()
-
+	
 	if menu.d.dq:Value() and IsReady(_Q) then
-		DrawCircle(GetOrigin(myHero),625,5,100,0xff00ff00)
+		DrawCircle(GetOrigin(myHero),625,5,80,0xff00ff00)
 	end
 	if menu.d.dw:Value() and IsReady(_W) then
-		DrawCircle(GetOrigin(myHero),625,5,100,0xff00ff00)
+		DrawCircle(GetOrigin(myHero),625,5,80,0xff00ff00)
 	end
 	if menu.d.dr:Value() and IsReady(_R) then
-		DrawCircle(GetOrigin(myHero),600,5,100,0xff00ff00)
+		DrawCircle(GetOrigin(myHero),600,5,80,0xff00ff00)
 	end
-	if menu.d.dmg:Value() and ValidTarget(target,2000) then
-		DrawDmgOverHpBar(target,enemyhp,0,apdmg,ARGB(180,255,255,255))
+	if menu.d.dmg:Value() and ValidTarget(target,2000) and IsReady(_Q) then
+		DrawDmgOverHpBar(target,enemyhp,0,Qdmg,ARGB(180,255,255,255))
+	elseif ValidTarget(target,2000) and IsReady(_W) then
+		DrawDmgOverHpBar(target,enemyhp,0,Wdmg,ARGB(180,255,255,255))
+	elseif ValidTarget(target,2000) and IsReady(_R) then
+		DrawDmgOverHpBar(target,enemyhp,0,Rdmg,ARGB(180,255,255,255))	
 	end
-end
+end)
